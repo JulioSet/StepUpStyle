8ec7952a7306cc351cf1b53cc7493cfbb955ffa4
carlaacrst <crystalitacarla@gmail.com>
2024-01-03 19:51:42 +0700

    Detail Product

--- routes/web.php
+++ routes/web.php
@@ -109,9 +109,12 @@ Route::get('/contact', [PageController::class, 'viewContact']);
 
 Route::prefix('products')->group(function () {
     Route::get('/', [PageController::class, 'viewAllProducts']);
+    Route::get('/{id}', [PageController::class, 'viewDetailProduct'])->name('product-detail');
     Route::get('/new-arrival', [PageController::class, 'viewNewArrival']);
     Route::get('/best-seller', [PageController::class, 'viewBestSeller']);
     Route::get('/flash-sale', [PageController::class, 'viewFlashSale']);
+    Route::get('/brand/{id}', [PageController::class, 'viewBrandProducts']);
+    Route::get('/category/{id}', [PageController::class, 'viewCategoryProducts']);
 });
 
 Route::prefix('cart')->group(function () {
