9927e807fa9bac6d08cf384c46f7e012e66c2c1a
Mike <Mike22setiawan@gmail.com>
2024-04-21 12:11:03 +0700

    Fix admin + sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -2,12 +2,11 @@
     use App\Models\kategori;
     use App\Models\supplier;
     use App\Models\DetailSepatu;
-	$userLoggedIn = Session::get('userLoggedIn');
-    $listCategory = kategori::All();
-    $listSupplier = supplier::All();
-    $listDetail = DetailSepatu::All();
-
-    $listUkuran = [36,37,38,39,40,41,42,43,44,45];
+    $userLoggedIn = Session::get('userLoggedIn');
+    $listCategory = kategori::all();
+    $listSupplier = supplier::all();
+    $listDetail = DetailSepatu::all();
+    $listUkuran = [36, 37, 38, 39, 40, 41, 42, 43, 44, 45];
     $listWarna = ['Hitam', 'Putih', 'Merah', 'Biru', 'Abu-abu', 'Coklat'];
 @endphp
 
@@ -17,63 +16,67 @@
         <ul class="main-categories">
             @forelse ($listCategory as $key=>$category)
                 <li class="main-nav-list">
-                    <a data-toggle="collapse" href="" aria-expanded="false" aria-controls="">
+                    <a href="{{ route('product-category', $category->kategori_id) }}">
                         <span class="lnr lnr-arrow-right"></span>
-                        <a href="{{ route('product-category', $category->kategori_id) }}">{{ $category->kategori_nama }}</a>
+                        {{ $category->kategori_nama }}
                     </a>
                 </li>
             @empty
-                <li class="main-nav-list"><a data-toggle="collapse" href="#fruitsVegetable" aria-expanded="false" aria-controls="fruitsVegetable"><span class="lnr lnr-arrow-right">
-                </span>No Category Yet</a>
+                <li class="main-nav-list">
+                    <a href="#fruitsVegetable" data-toggle="collapse" aria-expanded="false" aria-controls="fruitsVegetable">
+                        <span class="lnr lnr-arrow-right"></span>
+                        No Category Yet
+                    </a>
                 </li>
             @endforelse
         </ul>
     </div>
     <form action="{{ route('filter-products') }}" method="get">
-    @csrf
-    <div class="sidebar-filter mt-50">
-        <div class="top-filter-head">Product Filters</div>
-        <div class="common-filter">
-            <div class="head">Brands</div>
+        @csrf
+        <div class="sidebar-filter mt-50">
+            <div class="top-filter-head">Product Filters</div>
+            <div class="common-filter">
+                <div class="head">Brands</div>
                 <ul>
                     @forelse ($listSupplier as $key=>$supplier)
-                    <li class="filter-list">
-                        <input class="pixel-radio" type="checkbox"  name="brand[]" value="{{$supplier->supplier_id}}">
-                        <label for="{{$supplier->supplier_name}}">{{$supplier->supplier_name}}</label>
-                    </li>
+                        <li class="filter-list">
+                            <input class="pixel-radio" type="checkbox" name="brand[]" value="{{ $supplier->supplier_id }}">
+                            <label for="{{ $supplier->supplier_name }}">{{ $supplier->supplier_name }}</label>
+                        </li>
                     @empty
-
+                        
                     @endforelse
                 </ul>
-            </form>
-        </div>
-        <div class="common-filter">
-            <div class="head">Size</div>
+            </div>
+
+            <div class="common-filter">
+                <div class="head">Size</div>
                 <ul>
                     @forelse ($listUkuran as $key=>$ukuran)
-                    <li class="filter-list">
-                        <input class="pixel-radio" type="checkbox" id="{{ $ukuran }}" name="size[]" value="{{ $ukuran }}">
-                        <label for="{{ $ukuran }}">{{ $ukuran }}</label>
-                    </li>
+                        <li class="filter-list">
+                            <input class="pixel-radio" type="checkbox" id="{{ $ukuran }}" name="size[]" value="{{ $ukuran }}">
+                            <label for="{{ $ukuran }}">{{ $ukuran }}</label>
+                        </li>
                     @empty
-
+                        
                     @endforelse
                 </ul>
             </div>
 
-            <div class="head">Warna</div>
+            <div class="common-filter">
+                <div class="head">Warna</div>
                 <ul>
                     @forelse ($listWarna as $key=>$warna)
-                    <li class="filter-list">
-                        <input class="pixel-radio" type="checkbox" id="{{ $warna }}" name="color[]" value="{{ $warna }}">
-                        <label for="{{ $warna }}">{{ $warna }}</label>
-                    </li>
+                        <li class="filter-list">
+                            <input class="pixel-radio" type="checkbox" id="{{ $warna }}" name="color[]" value="{{ $warna }}">
+                            <label for="{{ $warna }}">{{ $warna }}</label>
+                        </li>
                     @empty
-
+                        
                     @endforelse
                 </ul>
             </div>
-            <button class="primary-btn" type="submit" style="width:100%;text-align:center">Filter</a>
-        </form>
-    </div>
+            <button class="primary-btn" type="submit" style="width:100%;text-align:center">Filter</button>
+        </div>
+    </form>
 </div>
